ef16e16ac58e2546ec3aef94a8b52ef08eb43146
lim3nka <emilborsic2008@gmail.com>
2025-03-24 07:59:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace knjiznicaa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            public int Id { get; set; }
+            public string Naslov { get; set; }
+            public string Autor { get; set; }
+            public string ISBN { get; set; }
+            public int Godina { get; set; }
+           public bool Dostupna { get; set; }
+
+        public Knjiga(int id, string naslov, string autor, string isbn, int godinaIzdavanja)
+        {
+
+            Id = id;
+            Naslov = naslov;
+            Autor = autor;
+            ISBN = isbn;
+            Godina = godinaIzdavanja;
+            Dostupna = true;
+
+        }
+    }
+}
+}
